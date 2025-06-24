08c8ea64649813b7d567381578b3532dee1c62ff
nguyenxuanbac88 <xuanbacatym@gmail.com>
2025-06-24 13:51:22 +0700

    -add logout API

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -28,6 +28,11 @@ namespace API_Project.Controllers
             var (status, message) = await _profileService.ChangePasswordAsync(model, HttpContext);
             return Ok(new { status, message });
         }
-
+        [HttpGet("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            var result = await _profileService.HandleLogoutAsync(HttpContext);
+            return result;
+        }
     }
 }
