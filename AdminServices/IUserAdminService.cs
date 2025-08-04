2f53eaf451b816fe936d714c656e463c49fa4cd6
nguyenxuanbac88 <xuanbacatym@gmail.com>
2025-08-04 14:13:51 +0700

    Feat: Manager User

---
+++ AdminServices/IUserAdminService.cs
@@ -0,0 +1,9 @@
+﻿using API_Project.Models.DTOs;
+
+namespace API_Project.AdminServices
+{
+    public interface IUserAdminService
+    {
+        Task<List<UserDto>> GetAllUsersAsync(string? keyword);
+    }
+}
---
+++ Controllers/AdminUserController.cs
@@ -0,0 +1,35 @@
+﻿using API_Project.AdminServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Project.Controllers
+{
+    [Route("api/admin/users")]
+    [ApiController]
+    [Authorize(Roles = "Admin")] // nếu có xác thực
+    public class AdminUserController : ControllerBase
+    {
+        private readonly IUserAdminService _userService;
+        private readonly IInvoiceService _invoiceService;
+
+        public AdminUserController(IUserAdminService userService, IInvoiceService invoiceService)
+        {
+            _userService = userService;
+            _invoiceService = invoiceService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllUsers([FromQuery] string? keyword)
+        {
+            var users = await _userService.GetAllUsersAsync(keyword);
+            return Ok(users);
+        }
+
+        [HttpGet("{userId}/invoices")]
+        public async Task<IActionResult> GetUserInvoices(int userId)
+        {
+            var invoices = await _invoiceService.Invoice(userId);
+            return Ok(invoices);
+        }
+    }
+}
