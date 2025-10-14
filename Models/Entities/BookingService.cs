d510643fed068d0d01b075033e4cbdb56b61f0d1
LtThanh <lamtanthanh2005@gmail.com>
2025-10-14 13:58:32 +0700

    remove BookingService in Models/Entities

--- Models/Entities/BookingService.cs
+++ Models/Entities/BookingService.cs
@@ -1,27 +1,27 @@
-﻿namespace API_Project.Models.Entities
-{
-    public class TicketBookingResultDto
-    {
-        public int IdInvoice { get; set; }
-        public int TotalPrice { get; set; }
-        public int PointUsed { get; set; }
-        public int PointEarned { get; set; }
-        public List<SeatInfoDto> Seats { get; set; }
-        public List<FoodInfoDto> Foods { get; set; }
-    }
+﻿// namespace API_Project.Models.Entities
+// {
+//     public class TicketBookingResultDto
+//     {
+//         public int IdInvoice { get; set; }
+//         public int TotalPrice { get; set; }
+//         public int PointUsed { get; set; }
+//         public int PointEarned { get; set; }
+//         public List<SeatInfoDto> Seats { get; set; }
+//         public List<FoodInfoDto> Foods { get; set; }
+//     }
 
-    public class SeatInfoDto
-    {
-        public string SeatCode { get; set; }
-        public int Price { get; set; }
-    }
+//     public class SeatInfoDto
+//     {
+//         public string SeatCode { get; set; }
+//         public int Price { get; set; }
+//     }
 
-    public class FoodInfoDto
-    {
-        public int IdFood { get; set; }
-        public string FoodName { get; set; }
-        public int Quantity { get; set; }
-        public int TotalPrice { get; set; }
-    }
+//     public class FoodInfoDto
+//     {
+//         public int IdFood { get; set; }
+//         public string FoodName { get; set; }
+//         public int Quantity { get; set; }
+//         public int TotalPrice { get; set; }
+//     }
 
-}
+// }
